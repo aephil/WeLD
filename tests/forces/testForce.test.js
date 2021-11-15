f4f7d24034679972a307fbbbed7d29d180d60056
Christoffer Aakre <christoffer.corfield@gmail.com>
2021-11-15 11:15:53 +0100

    refactored vtests. note the valence angle tests still only test the central force

--- tests/forces/testForce.test.js
+++ tests/forces/testForce.test.js
@@ -2,9 +2,9 @@ Physics = {};
 require('../../modules/physics/ForceMap');
 
 it("Test force returns (1, 1, 1)", () => {
-    const d = {};
+    const d = {id: 0};
     const data = [{}];
     const params = [];
-    const force = Physics.ForceMap["Test Force"](d, data, params);
-    expect(force).toEqual({x: 1, y: 1, z: 1});
+    const actions = Physics.ForceMap["Test Force"](d, data, params);
+    expect(actions).toEqual([[d.id, {x: 100, y: 100, z: 0}]]);
 })
--- tests/forces/valenceAngle.test.js
+++ tests/forces/valenceAngle.test.js
@@ -12,24 +12,34 @@ params = [1, Math.PI / 2, 0, 1];
 
 it("Object at origin should return 0 force", () => {
     const d = { ri: { x: 0, y: 0, z: 0 } };
-    const force = Physics.ForceMap.valenceAngle(d, data, params);
-    expect(force).toEqual({ x: -0, y: -0, z: 0 });
+    const actions = Physics.ForceMap.valenceAngle(d, data, params);
+    const fa = actions[0][1]
+    const fb = actions[1][1]
+    const fc = actions[2][1]
+
+    expect(fb).toEqual({ x: -0, y: -0, z: 0 });
 })
 
 it(`Force should have components x = y < 0, z = 0
     when angle is greater than 90 degrees`, () => {
     const d = { ri: { x: 0.1, y: 0.1, z: 0 } };
-    const force = Physics.ForceMap.valenceAngle(d, data, params);
-    expect(force.x).toEqual(force.y)
-    expect(force.x).toBeLessThan(0)
-    expect(force.z).toEqual(-0)
+    const actions = Physics.ForceMap.valenceAngle(d, data, params);
+    const fa = actions[0][1]
+    const fb = actions[1][1]
+    const fc = actions[2][1]
+    expect(fb.x).toEqual(fb.y)
+    expect(fb.x).toBeLessThan(0)
+    expect(fb.z).toEqual(-0)
 })
 
 it(`Force should have components x = y > 0, z < 0 since
     the angle is less than 90 degrees`, () => {
     const d = { ri: { x: 0, y: 0, z: 0.1 } };
-    const force = Physics.ForceMap.valenceAngle(d, data, params);
-    expect(force.x).toEqual(force.y);
-    expect(force.x).toBeGreaterThan(0);
-    expect(force.z).toBeLessThan(0)
+    const actions = Physics.ForceMap.valenceAngle(d, data, params);
+    const fa = actions[0][1]
+    const fb = actions[1][1]
+    const fc = actions[2][1]
+    expect(fb.x).toEqual(fb.y);
+    expect(fb.x).toBeGreaterThan(0);
+    expect(fb.z).toBeLessThan(0)
 })
