df9df0a0afa7d7c7d3e80fedb771229a784640b2
Christoffer Aakre <christoffer.corfield@gmail.com>
2021-11-17 12:22:31 +0100

    Revert "Tested. Added values to d and running the program"
    This reverts commit 6c30a1b578735217264fb0f6674f82d47286f45c.
    See my comments on the reverted commit. It adds some things
    we don't want like system-specific paths and copied code,
    and doesn't make any actual changes to the code.

--- tests/forces/testForce.test.js
+++ tests/forces/testForce.test.js
@@ -1,10 +1,10 @@
 Physics = {};
-require('"C:\Users\padma\Documents\GitHub\WeLD\modules\physics\ForceMap.js"');
+require('../../modules/physics/ForceMap');
 
 it("Test force returns (1, 1, 1)", () => {
-    const d = { id: 0 };
+    const d = {id: 0};
     const data = [{}];
     const params = [];
     const actions = Physics.ForceMap["Test Force"](d, data, params);
-    expect(actions).toEqual([[d.id, { x: 100, y: 100, z: 0 }]]);
+    expect(actions).toEqual([[d.id, {x: 100, y: 100, z: 0}]]);
 })
