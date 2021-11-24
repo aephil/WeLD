96f9fc3b7a9c33a55cde9bfc9d3a8f0d9621890e
Paddy198 <92857298+Paddy198@users.noreply.github.com>
2021-11-24 22:37:25 +0000

    Seems to be an issue with the lj potential, at origin. Screenshot has been attached to comment on github

--- tests/forces/valenceAngle.test.js
+++ tests/forces/valenceAngle.test.js
@@ -2,7 +2,7 @@ Physics = {};
 require('../../modules/physics/Vector');
 require('../../modules/physics/ForceMap');
 
-let data = [{id: 0, ri: { x: 1, y: 0, z: 0 } }, {id: 2, ri: { x: 0, y: 1, z: 0 } }];
+let data = [{ id: 0, ri: { x: 1, y: 0, z: 0 } }, { id: 2, ri: { x: 0, y: 1, z: 0 } }];
 
 /// presumably this is a function or maybe a var (required verif)consisting of real coordinates of force
 params = [1, Math.PI / 2, 0, 1];
@@ -11,18 +11,21 @@ params = [1, Math.PI / 2, 0, 1];
 // should be zero force since the angle is already 90º
 
 it("Object at origin should return 0 force", () => {
-    const d = {id: 1, ri: { x: 0, y: 0, z: 0 } };
+    const d = { id: 1, ri: { x: 0, y: 0, z: 0 } };
     const actions = Physics.ForceMap.valenceAngle(d, data, params);
     const fa = actions[0][1]
     const fb = actions[1][1]
     const fc = actions[2][1]
 
     expect(fb).toEqual({ x: -0, y: -0, z: 0 });
+    expect(fa).toEqual({ x: 0, y: 0, z: 0 });
+    expect(fc).toEqual({ x: 0, y: 0, z: 0 });
+
 })
 
 it(`Force should have components x = y < 0, z = 0
     when angle is greater than 90 degrees`, () => {
-    const d = {id: 1, ri: { x: 0.1, y: 0.1, z: 0 } };
+    const d = { id: 1, ri: { x: 0.1, y: 0.1, z: 0 } };
     const actions = Physics.ForceMap.valenceAngle(d, data, params);
     const fa = actions[0][1]
     const fb = actions[1][1]
@@ -30,11 +33,12 @@ it(`Force should have components x = y < 0, z = 0
     expect(fb.x).toEqual(fb.y)
     expect(fb.x).toBeLessThan(0)
     expect(fb.z).toEqual(-0)
+    expect(fa.x).toEqual(fc.y)
 })
 
 it(`Force should have components x = y > 0, z < 0 since
     the angle is less than 90 degrees`, () => {
-    const d = {id: 1, ri: { x: 0, y: 0, z: 0.1 } };
+    const d = { id: 1, ri: { x: 0, y: 0, z: 0.1 } };
     const actions = Physics.ForceMap.valenceAngle(d, data, params);
     const fa = actions[0][1]
     const fb = actions[1][1]
@@ -43,3 +47,5 @@ it(`Force should have components x = y > 0, z < 0 since
     expect(fb.x).toBeGreaterThan(0);
     expect(fb.z).toBeLessThan(0)
 })
+
+// it('Force should have components x ')
