0209522b90aa108ee98f716b995203b49e150f12
Christoffer Aakre <christoffer.corfield@gmail.com>
2021-12-14 19:04:04 +0100

    Weird behaviour with spring potential

---
+++ simulations/spring/testSpring2.js
@@ -0,0 +1,19 @@
+const edgeLen = 20;
+lattice.makePrimitive3D(3,3,1,edgeLen);
+
+const k = 1;
+
+lattice.setInterAtomicForces(
+{
+ name: "spring",
+ params: [k /* spring constant k */, edgeLen /* equilibrium separation*/],
+ color: "red" // currently not in use.
+},
+springPredicate // depending on the predicate sets the neighbour in params.
+);
+
+// TESTING: push the first node in the x direction to simulate an initial Extension
+lattice.data[1].ri.x += 10;
+
+function debug() {
+}
