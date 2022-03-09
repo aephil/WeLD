476c9321f54f4286c55d5309df8f09056b1db7b9
Christoffer Aakre <christoffer.corfield@gmail.com>
2022-03-09 11:42:48 +0100

    fixed valance angle unit tests

---
+++ src/simulations/valance/valanceAngle.js
@@ -0,0 +1,93 @@
+import Physics from '../../namespaces/Physics.js';
+import Graphics from '../../namespaces/Graphics.js';
+import {
+    calculateQuantities,
+    KineticEnergy,
+    PotentialEnergy
+} from '../../modules/physics/quantities.js';
+
+var shared = {};
+const edgeLen = 20;
+
+const ui = new Graphics.UserInterface(shared);
+ui.loadBasic(); // loads divs for simulation, control and terminal, and initialises the terminal
+
+const lattice = new Physics.Lattice(shared);
+lattice.setShowEdges(true);
+
+// Spring neighbour predicate
+const springPredicate = (d1, d2) => {
+    if (d1.id === d2.id) return false;
+
+    const dx2 = (d2.ri.x - d1.ri.x) ** 2;
+    const dy2 = (d2.ri.y - d1.ri.y) ** 2;
+    const dz2 = (d2.ri.z - d1.ri.z) ** 2;
+    const distanceSquared = dx2 + dy2 + dz2;
+
+    return distanceSquared <= edgeLen ** 2;
+}
+
+lattice.makePrimitive3D(2, 1, 1, edgeLen);
+
+const k = 1;
+
+lattice.setInterAtomicForces(
+    {
+        name: "spring",
+        params: [k /* spring constant k */, edgeLen /* equilibrium separation*/],
+        color: "red" // currently not in use.
+    },
+    springPredicate // depending on the predicate sets the neighbour in params.
+);
+
+// TESTING: push the first node in the x direction to simulate an initial Extension
+lattice.sharedData.nodes[0].ri.x += 10;
+
+
+let i = 0;
+function meanOf(arr) {
+    const sum = arr.reduce((a, b) => a + b, 0);
+    const mean = (sum / arr.length) || 0;
+    return mean;
+}
+
+function debugQuantities(lattice) {
+    if (i % 100 === 0) {
+
+        ui.clearTerminal();
+        ui.logDebug('----------------------')
+
+        // energies /////////////////////////////////////////////////
+
+        const KE = lattice.quantities[0].value;
+        const PE = lattice.quantities[1].value;
+        const TE = KE + PE;
+        ui.logDebug(`Kinetic energy: ${KE}`);
+        ui.logDebug(`Potential energy: ${PE}`);
+        ui.logDebug(`Total energy: ${TE}`);
+        ui.logDebug('----------------------');
+
+    }
+    i += 1;
+}
+
+lattice.setQuantities([
+    new KineticEnergy(),
+    new PotentialEnergy()
+]);
+
+const verletController = Physics.verlet;
+const updates = [verletController.integrationStep, calculateQuantities]
+const nodeUpdates = [];
+
+const renderer = new Graphics.Renderer(shared);
+
+renderer.setUI(ui);
+//renderer.setLattice(lattice);
+renderer.setUpdates(updates);
+//renderer.setNodeUpdates(nodeUpdates);
+//renderer.setDebug(debugQuantities);
+
+renderer.setFPS(30);
+renderer.setSpeed(1000);
+renderer.render()
--- tests/forces/valenceAngle.test.js
+++ tests/forces/valenceAngle.test.js
@@ -1,18 +1,21 @@
 const Vector = require('../../transpiled/modules/physics/Vector');
 const ForceMap = require('../../transpiled/modules/physics/ForceMap')
 
-let data = [{ id: 0, ri: { x: 1, y: 0, z: 0 } }, { id: 2, ri: { x: 0, y: 1, z: 0 } }];
+let shared =
+    {
+        nodes:  [
+            { id: 0, ri: { x: 1, y: 0, z: 0 } },
+            { id: 1, ri: { x: 0, y: 0, z: 0 } },
+            { id: 2, ri: { x: 0, y: 1, z: 0 }
+            }]
+    };
 
-/// presumably this is a function or maybe a var (required verif)consisting of real coordinates of force
-let params = [1, Math.PI / 2, 0, 1];
-
-/// This is the var storing angle data for the atom
-// should be zero force since the angle is already 90º
+let params = [1, Math.PI / 2, 0, 2];
 
 describe("Valence angle interaction", () => {
     it("Object at origin should return 0 force", () => {
-        const d = { id: 1, ri: { x: 0, y: 0, z: 0 } };
-        const actions = ForceMap.valenceAngle(d, data, params);
+        const d = shared.nodes[1]
+        const actions = ForceMap.valenceAngle(d, shared, params);
         const fa = actions[0][1]
         const fb = actions[1][1]
         const fc = actions[2][1]
@@ -25,7 +28,7 @@ describe("Valence angle interaction", () => {
     it(`Force should have components x = y < 0, z = 0
         when angle is greater than 90 degrees`, () => {
         const d = { id: 1, ri: { x: 0.1, y: 0.1, z: 0 } };
-        const actions = ForceMap.valenceAngle(d, data, params);
+        const actions = ForceMap.valenceAngle(d, shared, params);
         const fa = actions[0][1]
         const fb = actions[1][1]
         const fc = actions[2][1]
@@ -38,7 +41,7 @@ describe("Valence angle interaction", () => {
     it(`Force should have components x = y > 0, z < 0 since
         the angle is less than 90 degrees`, () => {
         const d = { id: 1, ri: { x: 0, y: 0, z: 0.1 } };
-        const actions = ForceMap.valenceAngle(d, data, params);
+        const actions = ForceMap.valenceAngle(d, shared, params);
         const fa = actions[0][1]
         const fb = actions[1][1]
         const fc = actions[2][1]
